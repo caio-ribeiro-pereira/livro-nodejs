c2141febd14244fa23ba8278c800bb1454ead072
Caio Ribeiro Pereira <caio.ribeiro.pereira@gmail.com>
2017-10-11 00:46:30 -0300

    updated cap6

--- capitulo-6/ntalk/app.js
+++ capitulo-6/ntalk/app.js
@@ -4,22 +4,45 @@ const http = require('http');
 const socketIO = require('socket.io');
 const consign = require('consign');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const cookie = require('cookie');
 const expressSession = require('express-session');
 const methodOverride = require('method-override');
+const config = require('./config');
 const error = require('./middlewares/error');
+
 const app = express();
 const server = http.Server(app);
 const io = socketIO(server);
+const store = new expressSession.MemoryStore();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cookieParser('ntalk'));
-app.use(expressSession({ resave: true, saveUninitialized: true, secret: 'ntalk' }));
+app.use(expressSession({
+  store,
+  resave: true,
+  saveUninitialized: true,
+  name: config.sessionKey,
+  secret: config.sessionSecret
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+
+io.use((socket, next) => {
+  const cookieData = socket.request.headers.cookie;
+  const cookieObj = cookie.parse(cookieData);
+  const sessionHash = cookieObj[config.sessionKey] || '';
+  const sessionID = sessionHash.split('.')[0].slice(2);
+  store.all((err, sessions) => {
+    const currentSession = sessions[sessionID];
+    if (err || !currentSession) {
+      return next(new Error('Acesso negado!'));
+    }
+    socket.handshake.session = currentSession;
+    return next();
+  });
+});
 
 consign({})
   .include('models')
@@ -32,6 +55,4 @@ consign({})
 app.use(error.notFound);
 app.use(error.serverError);
 
-server.listen(3000, () => {
-  console.log('Ntalk no ar.');
-});
+server.listen(3000, () => console.log('Ntalk no ar.'));
--- capitulo-6/ntalk/events/chat.js
+++ capitulo-6/ntalk/events/chat.js
@@ -1,9 +1,33 @@
 module.exports = (app, io) => {
+  const onlines = {};
   io.on('connection', (client) => {
-    client.on('send-server', (data) => {
-      const msg = `<b>${data.nome}:</b> ${data.msg}<br>`;
-      client.emit('send-client', msg);
-      client.broadcast.emit('send-client', msg);
+    const { session } = client.handshake;
+
+    client.on('send-server', (msg) => {
+      const { usuario, sala } = session;
+      const resposta = `<b>${usuario.nome}:</b> ${msg}<br>`;
+      const novaMensagem = { email: usuario.email, sala };
+      onlines[usuario.email] = usuario.email;
+      for (let email in onlines) {
+        client.emit('notify-onlines', email);
+        client.broadcast.emit('notify-onlines', email);
+      }
+      io.to(sala).emit('send-client', resposta);
+      client.broadcast.emit('new-message', novaMensagem);
+    });
+
+    client.on('create-room', (sala) => {
+      session.sala = sala;
+      client.join(sala);
+    });
+
+    client.on('disconnect', () => {
+      const { usuario, sala } = session;
+      const resposta = `<b>${usuario.nome}:</b> saiu.<br>`;
+      delete onlines[usuario.email];
+      client.broadcast.emit('notify-offlines', usuario.email);
+      io.to(sala).emit('send-client', resposta);
+      client.leave(sala);
     });
   });
 };
